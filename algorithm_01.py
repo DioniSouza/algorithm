4fec774cdb46687a8838d4b15627ab29066614c3
DioniSouza <151585681+DioniSouza@users.noreply.github.com>
2024-10-27 12:44:07 -0300

    Primeiros códigos do caderno de algorithm

---
+++ algorithm_01.py
@@ -0,0 +1,40 @@
+#Calcule o valor de cada expressão abaixo e indique o tipo do resultado (inteiro ou real)
+
+
+import math
+
+a = (20 - 15 / 2)
+print (type(a))
+
+b = 20 - 15 / 2
+print (type(b))
+
+c = 2 * 5 / 20 + 30 / 15 * 2
+print (type(c))
+
+d = 2 *(5 / 20) + 30 / (15 * 2)
+print (type(d))
+
+e = 23 // 4
+print (type(e))
+
+f = 23 % 4
+print (type(f))
+
+g = 35 // 6 + 2
+print (type(g))
+
+h = 35 // 6 - 2 
+print (type(h))
+
+i = 35 // 6 * 2
+print (type(i))
+
+j = math.sqrt(625)
+print (type(j))
+
+k = 20**2
+print (type(k))
+
+l = 2 + math.sqrt(21//5)
+print (type(l))
---
+++ algorithm_02.py
@@ -0,0 +1,17 @@
+#Escreva em pseudocódigo, as seguintes expressões matemáticas:
+
+#Obter os valores
+a = input("Digite um valor para A: ")
+b = input("Digite um valor para B: ")
+c = input("Digite um valor para C: ")
+
+#Converter os valores
+a = int(a)
+b = int(b)
+c = int(c)
+
+#Calcular a expressão
+ex01 = (a + b) * c
+
+#Imprimir o resultado
+print ("O valor da expressão (A+B)xC é: ", ex01)
