30ca0133fd58ceb3732e5b0b24dd84921fc5dcd5
DioniSouza <151585681+DioniSouza@users.noreply.github.com>
2024-11-01 13:08:31 -0300

    hoje não deu, amanhã talvez

--- exercicio_30(comissão).py
+++ exercicio_30(comissão).py
@@ -1,12 +1,35 @@
 #Uma empresa de vendas tem três corretores. A empresa paga ao corretor uma comissão calculada de acordo com o valor de suas vendas. Se o valor da venda de um corretor for maior que R$ 50.000.00 a comissão será de 12% do valor vendido. Se o valor da venda do corretor estiver entre R$ 30.000.00 e R$ 50.000.00 (incluindo extremos) a comissão será de 9.5%. Em qualquer outro caso, a comissão será de 7%. Escreva um algoritmo que gere um relatório contendo nome, valor da venda e comissão de cada um dos corretores. O relatório deve mostrar também o total de vendas da empresa
 
-#Variáveis
-corretor_1 = str(input("Digite o nome do primeiro corretor: "))
-resultado_1 = float(input("Insira o resultado do primeiro corretor: "))
-corretor_2 = str(input("Digite o nome do segundo corretor: "))
-resultado_2 = float(input("Insira o resultado do segundo corretor: ")) 
-corretor_3 = str(input("Digite o nome do terceiro corretor: "))
-resultado_3 = float(input("Insira o resultado do terceiro corretor: "))
-total_vendas = resultado_1 + resultado_2 + resultado_3 
-#Resolução
-if 
+#lista de corretores
+corretores = []
+
+#loop
+for i in range(3):
+    nome = str(input(f"Digite o nome do corretor {i+1}: "))
+    valor_venda = float(input(f"Digite o resultado do Corretor {i+1}: "))
+
+#Calcúlo
+if valor_venda > 50000:
+    comissao = valor_venda * 0.12
+elif valor_venda >=30000:
+    comissao = valor_venda * 0.095
+else:
+    comissao = valor_venda * 0.075
+    
+#Adicionar os dados do corretor a lista
+corretores.append({"nome":nome, "valor_venda": valor_venda, "comissao": comissao})
+
+#total de vendas
+total_vendas = 0
+for corretor in corretores:
+    total_vendas += corretor["valor_venda"]
+    
+#imprimindo resultado
+print("Relatório de Vendas:")
+print("-" * 30)
+print("Corretor | Valor da Venda | Comissão")
+print("-" *30)
+for corretor in corretores:
+    print(f"{corretor['nome']} | R$ {corretor['valor_venda']:.2f} | R$ {corretor['comissao']:.2f}")
+    print("-" * 30)
+    print(f"Total de Vendas: R$ {total_vendas:}")
