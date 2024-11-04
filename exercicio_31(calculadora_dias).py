d657a18dba30761c4c0e1a297b87471d45bd1783
DioniSouza <151585681+DioniSouza@users.noreply.github.com>
2024-11-04 12:35:02 -0300

    seguindo

--- exercicio_31(calculadora_dias).py
+++ exercicio_31(calculadora_dias).py
@@ -1,4 +1,20 @@
 #Escreva um algoritmo que determine o número de dias que uma pessoa já viveu. Considere que um mês tenha 30 dias.
-
+from datetime import date
 
 # Variáveis
+dia_nasc = int(input('Digite seu dia de nascimento: '))
+mes_nasc = int(input('Digite seu mês de nascimento: '))
+ano_nasc = int(input('Digite seu ano de nascimento: '))
+
+
+
+# Calculando o dia, mês e ano atual
+dia_atual = date.today().day
+mes_atual = date.today().month
+ano_atual = date.today().year
+
+# Calculando a idade em dias
+idade_dias = (ano_atual - ano_nasc) * 365 + (mes_atual - mes_nasc) * 30 + (dia_atual - dia_nasc)
+
+# Apresentando a idade em dias
+print('Você já viveu', idade_dias, 'dias.')
---
+++ exercicio_32(comparador).py
@@ -0,0 +1,12 @@
+    # Reunião de dados
+a = float(input("Digite um valor para A: "))
+b = float(input("Digite um valor para B: "))
+c = float(input("Digite um valor para C: "))
+
+    # Comparação
+if a + b < c:
+        print("A soma de A e B é menor que C")
+elif a + b > c:
+       print("A soma de A e B é maior que C")
+else:
+        print("A soma de A e B é igual a C")
